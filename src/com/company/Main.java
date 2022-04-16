9c0e558bf33efde39fb49e6d292624d41a63f3bb
Harshaka89 <buddhinathvitharana@gmail.com>
2022-04-16 23:27:53 +0200

    add 4 sout

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("myapp");
-       
+        System.out.println("4myapp");
+        System.out.println("3myapp");
+        System.out.println("2myapp");
+        System.out.println("1myapp");
+
     }
 }
