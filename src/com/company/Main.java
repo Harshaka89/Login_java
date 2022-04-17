16d4397145724306e79a31b883b12cc1d865f330
Harshaka89 <buddhinathvitharana@gmail.com>
2022-04-17 14:51:52 +0200

    reset main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("4myapp");
-        System.out.println("3myapp");
-        System.out.println("2myapp");
-        System.out.println("1myapp");
+
 
     }
 }
